56db8f5b9df665a6f7a380ec593cb4f1c8703915
PratishaBista <bista.pratisha@gmail.com>
2026-08-31 20:11:48 +0545

    Configure gunicorn and dynamic port for Render deployment

--- app.py
+++ app.py
@@ -83,4 +83,8 @@ def suggest():
     return jsonify(suggestions[:10])
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
+
